837ea588b1d361c90ce6a2d1c2f22c135be50efb
samazon <letcsam@gmail.com>
2023-01-08 15:53:05 +0500

    useLocalStorage

---
+++ useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [storedValue]);
+
+  return [storedValue, setStoredValue];
+}
